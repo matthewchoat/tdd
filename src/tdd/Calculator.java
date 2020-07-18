83454ebaa60f5b37a53e799cd93ddd84de3bec33
Matthew Choat <matthewchoat@gmail.com>
2020-07-18 13:28:37 -0500

    wrote add taxes test - pass

--- src/tdd/Calculator.java
+++ src/tdd/Calculator.java
@@ -11,7 +11,10 @@ public class Calculator {
 
   public BigDecimal addTaxes() {
     BigDecimal temp = null;
-
+      for(IProduct product:basket.getProductsInBasket()){
+        temp = product.getPrice();
+      }
+      temp = temp.multiply(BigDecimal.valueOf(1.10)).setScale(2, RoundingMode.HALF_EVEN);
     return temp;
   }
 
