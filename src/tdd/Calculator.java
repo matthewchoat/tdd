28a7f888f2491854ed01f1ae470bc06305803894
Matthew Choat <matthewchoat@gmail.com>
2020-07-19 11:47:09 -0500

    added logic for tax conditions - CalculatorTests - failing

--- src/tdd/Calculator.java
+++ src/tdd/Calculator.java
@@ -10,14 +10,33 @@ public class Calculator {
   public Calculator(){}
 
   public BigDecimal addTaxes(ArrayList<IProduct> productsCart) {
-    BigDecimal priceAddTax = null;
+
     BigDecimal totalTaxes = BigDecimal.valueOf(0.00);
       for(IProduct product : productsCart){
-        totalTaxes = totalTaxes.add(product.getPrice().multiply(Constants.TAX_RATE)).setScale(2, RoundingMode.HALF_EVEN);
-        priceAddTax = product.getPrice().multiply(Constants.TAX_MULTIPLIER).setScale(2, RoundingMode.HALF_EVEN);
-        product.setPrice(priceAddTax);
+        if (!product.isExempt() && !product.isImported()) {
+          totalTaxes = totalTaxes.add(product.getPrice().multiply(Constants.TAX_RATE)).setScale(2, RoundingMode.HALF_EVEN);
+              product.setPrice(product.getPrice().multiply(Constants.TAX_MULTIPLIER).setScale(2, RoundingMode.HALF_EVEN));
 
-        System.out.println("price add tax "+ priceAddTax + " price total tax " + totalTaxes + " New price " + product.getPrice());
+        }
+
+        else if (!product.isExempt() && product.isImported()) {
+          totalTaxes = totalTaxes.add(product.getPrice().multiply(Constants.TAXANDIMPORT_RATE)).setScale(2, RoundingMode.HALF_EVEN);
+              product.setPrice(product.getPrice().multiply(Constants.TAXANDIMPORT_MULTIPLIER).setScale(2, RoundingMode.HALF_EVEN));
+
+        }
+
+        else if (product.isExempt() && product.isImported()){
+          totalTaxes = totalTaxes.add(product.getPrice().multiply(Constants.IMPORT_MULTIPLIER)).setScale(2, RoundingMode.HALF_EVEN);
+              product.setPrice(product.getPrice().multiply(Constants.IMPORT_MULTIPLIER).setScale(2, RoundingMode.HALF_EVEN));
+        }
+
+        else{
+          //Is exempt, not imported = $0.00 tax
+        }
+
+
+
+        System.out.println(" New price " + product.getPrice() + " price total tax " + totalTaxes );
       }
     return totalTaxes;
   }
--- src/tdd/Constants.java
+++ src/tdd/Constants.java
@@ -3,6 +3,9 @@ package tdd;
 import java.math.BigDecimal;
 
 public class Constants {
-  public static final BigDecimal TAX_RATE = BigDecimal.valueOf(.10), IMPORT_RATE = BigDecimal.valueOf(.05),  TAX_MULTIPLIER = BigDecimal.valueOf(1.10), IMPORT_MULTIPLIER = BigDecimal.valueOf(1.05);
+  public static final BigDecimal TAXANDIMPORT_RATE = BigDecimal.valueOf(.15), TAX_RATE = BigDecimal.valueOf(.10), IMPORT_RATE = BigDecimal.valueOf(.05),
+      TAXANDIMPORT_MULTIPLIER = BigDecimal.valueOf(1.15), TAX_MULTIPLIER = BigDecimal.valueOf(1.10), IMPORT_MULTIPLIER = BigDecimal.valueOf(1.05);
+
+
 
 }
